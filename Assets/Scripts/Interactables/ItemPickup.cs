c637a77edd3fe1b883daf8c6e6cd140e138f2c6c
Izzy <1250423717@qq.com>
2025-05-14 14:25:51 +0800

    wheelbarrow & UI update

--- Assets/Scripts/Interactables/ItemPickup.cs
+++ Assets/Scripts/Interactables/ItemPickup.cs
@@ -44,7 +44,7 @@ public partial class ItemPickup : Area3D
 
     public async override void _PhysicsProcess(double delta)
     {
-        if (Input.IsActionJustPressed("action_use") && _isColliding && !_player.IsUsingWheelbarrow)
+        if (Input.IsActionJustPressed("action_use") && _isColliding)
         {
             Inventory inventory = (Inventory)GetTree().GetFirstNodeInGroup("inventory");
             inventory.AddItem(_item, _quantity);
